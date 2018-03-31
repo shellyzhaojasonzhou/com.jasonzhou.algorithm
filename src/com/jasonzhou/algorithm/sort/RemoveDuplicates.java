07aad31956db167c57b0a4039892f5ae3c3a0568
Administrator <Administrator@SC-201803311537>
2018-03-31 22:54:02 +0800

    author:jasonzhou reason:new project

---
+++ src/com/jasonzhou/algorithm/sort/RemoveDuplicates.java
@@ -0,0 +1,15 @@
+package com.jasonzhou.algorithm.sort;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RemoveDuplicates {
+
+	public static void main(String[] args) {
+		//组装传入指标数据
+		Map<String,List<String>> indexRelationMap = new LinkedHashMap<String,List<String>>();
+		//indexRelationMap.put(key, value)
+	}
+
+}
