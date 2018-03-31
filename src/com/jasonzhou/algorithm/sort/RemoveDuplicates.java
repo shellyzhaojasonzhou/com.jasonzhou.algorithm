c704f8b0ed439daa06fbbb766f317812bdda5d53
Administrator <Administrator@SC-201803311537>
2018-03-31 23:07:39 +0800

    添加指标关系

--- src/com/jasonzhou/algorithm/sort/RemoveDuplicates.java
+++ src/com/jasonzhou/algorithm/sort/RemoveDuplicates.java
@@ -1,5 +1,7 @@
 package com.jasonzhou.algorithm.sort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,7 +11,11 @@ public class RemoveDuplicates {
 	public static void main(String[] args) {
 		//组装传入指标数据
 		Map<String,List<String>> indexRelationMap = new LinkedHashMap<String,List<String>>();
-		//indexRelationMap.put(key, value)
+		indexRelationMap.put("1001", new ArrayList<String>(Arrays.asList("30001","30002","30003")));
+		indexRelationMap.put("1002", new ArrayList<String>(Arrays.asList("30003","30004","30009","30005")));
+		indexRelationMap.put("1003", new ArrayList<String>(Arrays.asList("30005","30006","30007")));
+		
+		//List<String> 
+		
 	}
-
 }
